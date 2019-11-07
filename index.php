ae1ac3319326a9e682421a2105e219967ba9fbe0
syukur <kurniasyukur@gmail.com>
2019-11-07 15:05:45 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Coba GIT remot lagi</title>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus corporis ullam a odit eaque voluptates accusantium, laborum dolorem optio laudantium, magnam modi totam cupiditate repellendus. Magnam iste ipsa reiciendis in!</p>
 </head>
 <body>
     
